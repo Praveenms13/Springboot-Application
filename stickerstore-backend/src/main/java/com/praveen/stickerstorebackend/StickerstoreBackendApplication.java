f49fa6f51ceba65b77c5c5e908c0e66307c7fb04
Praveen <mspraveenkumar77@gmail.com>
2025-07-17 09:17:51 +0530

    Initial Commit

---
+++ stickerstore-backend/src/main/java/com/praveen/stickerstorebackend/StickerstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.stickerstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickerstoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StickerstoreBackendApplication.class, args);
+    }
+
+}
---
+++ stickerstore-backend/src/test/java/com/praveen/stickerstorebackend/StickerstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.stickerstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickerstoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
