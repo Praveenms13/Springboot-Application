b5a0defe240ecd688a7960af7c00001fd286a92a
Praveen <mspraveenkumar77@gmail.com>
2025-07-31 20:59:00 +0530

    Implemented dropdown filter

--- ecommerce-ui/src/components/Dropdown.jsx
+++ ecommerce-ui/src/components/Dropdown.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-export default function Dropdown({ label, options, selectedValue, onChange }) {
+export default function Dropdown({
+  label,
+  options,
+  selectedValue,
+  handleSort,
+}) {
   return (
     <div className="flex items-center gap-2 justify-end pr-12 flex-1 font-primary">
       <label className="text-lg font-semibold text-primary">{label}</label>
       <select
         value={selectedValue}
-        onChange={onChange}
+        onChange={(event) => handleSort(event.target.value)}
         className="px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent text-sm bg-white"
       >
         {options.map((optionVal, index) => (
--- ecommerce-ui/src/components/ProductListings.jsx
+++ ecommerce-ui/src/components/ProductListings.jsx
@@ -3,22 +3,41 @@ import ProductCard from "./ProductCard";
 import SearchBox from "./SearchBox";
 import Dropdown from "./Dropdown";
 
-const sortList = ["Price: Low to High", "Price: High to Low", "Newest"];
+const sortList = ["Popularity", "Price: Low to High", "Price: High to Low"];
+
 export default function ProductListings({ products }) {
   const [searchText, setSearchText] = useState("");
+  const [selectedSort, setSelectedSort] = useState("Popularity");
 
-  function handleSearch(inputSearch) {
-    setSearchText(inputSearch);
-  }
+  const handleSearch = (inputSearch) => setSearchText(inputSearch);
+  const handleSort = (sortType) => setSelectedSort(sortType);
 
   let filteredAndSortedProducts = Array.isArray(products)
-    ? products.filter((product) => {
-        return (
+    ? products.filter(
+        (product) =>
           product.name.toLowerCase().includes(searchText.toLowerCase()) ||
           product.description.toLowerCase().includes(searchText.toLowerCase())
-        );
-      })
+      )
     : [];
+
+  switch (selectedSort) {
+    case "Price: Low to High":
+      filteredAndSortedProducts.sort(
+        (a, b) => parseFloat(a.price) - parseFloat(b.price)
+      );
+      break;
+    case "Price: High to Low":
+      filteredAndSortedProducts.sort(
+        (a, b) => parseFloat(b.price) - parseFloat(a.price)
+      );
+      break;
+    case "Popularity":
+    default:
+      filteredAndSortedProducts.sort(
+        (a, b) => parseInt(b.popularity) - parseInt(a.popularity)
+      );
+      break;
+  }
 
   return (
     <div className="max-w-[1152px] mx-auto">
@@ -27,9 +46,14 @@ export default function ProductListings({ products }) {
           label="Search"
           placeholder="Search by name or category"
           value={searchText}
-          handleSearch={(value) => handleSearch(value)}
+          handleSearch={handleSearch}
         />
-        <Dropdown label="Sort By" options={sortList} selectedValue="" />
+        <Dropdown
+          label="Sort By"
+          options={sortList}
+          selectedValue={selectedSort} // or value={selectedSort}
+          handleSort={handleSort}
+        />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
         {filteredAndSortedProducts.length > 0 ? (
@@ -38,7 +62,7 @@ export default function ProductListings({ products }) {
           ))
         ) : (
           <p className="text-center font-primary font-bold text-lg text-primary">
-            No products found
+            No products found 
           </p>
         )}
       </div>
